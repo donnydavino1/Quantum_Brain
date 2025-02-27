e7539d50b6008be4718ff64a150a0bc64de21a0e
donnydavino1 <133409191+donnydavino1@users.noreply.github.com>
2025-02-27 14:59:08 -0500

    Adding pulse sequence Files

--- phase.py
+++ phase.py
@@ -4,21 +4,38 @@ from numpy.typing import NDArray
 from scipy import integrate
 
 
-def autophase_all(spectra_complex: NDArray,
-                  state_name: str,
-                  phase_width: int = 200) -> NDArray:
-    l = spectra_complex.shape[0] // 2
+def auto_phase_all_spectra(ffts_avg: NDArray, phase_width: int, state_name: str):
+    """
+    Automatic 0th order phase. A bit hard coded since we use the P1 X1 imag spectra (L+R = I_1z, L-R = I_1z I_2z) and
+    P2 X2 imag spectra (L+R = I_2z, L-R = I_1z I_2z) to phase all the P1 and P2 spectra (respectively) since we
+    `know' theoretically what they should look like. (ex. the '1000' up-up state should have only absorption peaks in
+    P1X1 and P2X2 spectra.
+    :param ffts_avg:
+    :param phase_width:
+    :param state_name:
+    :return:
+    """
+    mid = ffts_avg.shape[0] // 2
 
-    spectrum_p1x1 = spectra_complex[1]  # P1 X1 (L+R = I_1z, L-R = I_1z I_2z)
+    if state_name == "1000":
+        PH0_P1_OFFSET, PH0_P2_OFFSET = 0, 0
+    elif state_name == "0100":
+        PH0_P1_OFFSET, PH0_P2_OFFSET = 0, 180
+    elif state_name == "0010":
+        PH0_P1_OFFSET, PH0_P2_OFFSET = 180, 0
+    elif state_name == "0001":
+        PH0_P1_OFFSET, PH0_P2_OFFSET = 180, 180
+    else:
+        raise ValueError(f"Unknown state_name: {state_name}. Should be one of ('1000', '0100', '0010', '0001')")
+
+    spectrum_p1x1 = ffts_avg[1]  # P1 X1 (L+R = I_1z, L-R = I_1z I_2z)
     ph_0_p1 = autophase_0(spectrum_p1x1, phase_width)
-
-    spectrum_p2x2 = spectra_complex[10]  # P2 X2 (L+R = I_2z, L-R = I_1z I_2z)
+    ffts_avg[:mid] = np.array(
+        [ng.proc_base.ps(s, p0=ph_0_p1 + PH0_P1_OFFSET, p1=0) for s in ffts_avg[:mid]])
+    spectrum_p2x2 = ffts_avg[10]  # P2 X2 (L+R = I_2z, L-R = I_1z I_2z)
     ph_0_p2 = autophase_0(spectrum_p2x2, phase_width)
-
-    # if ("1000" in state_name) and ("CN" not in state_name):
-
-    spectra_complex[:l] = np.array([ng.proc_base.ps(s, p0=ph_0_p1 + 90, p1=0) for s in spectra_complex[:l]])
-    spectra_complex[l:] = np.array([ng.proc_base.ps(s, p0=ph_0_p2 + 90, p1=0) for s in spectra_complex[l:]])
+    ffts_avg[mid:] = np.array(
+        [ng.proc_base.ps(s, p0=ph_0_p2 + PH0_P2_OFFSET, p1=0) for s in ffts_avg[mid:]])
 
 
 def autophase_0(spectrum: NDArray,
@@ -44,7 +61,6 @@ def autophase_0(spectrum: NDArray,
             area_best = area_curr
 
     return ph0_best
-
 
 # def autophase_0(spectrum: NDArray,
 #                 width: int = None,
@@ -81,3 +97,20 @@ def autophase_0(spectrum: NDArray,
 #     ph0_mean = (ph0_real + ph0_imag) / 2
 #     print(f"ph0_real = {ph0_real}, ph0_imag = {ph0_imag}. Average: {ph0_mean}")
 #     return ph0_mean
+
+
+# def autophase_all(spectra_complex: NDArray,
+#                   state_name: str,
+#                   phase_width: int = 200) -> NDArray:
+#     l = spectra_complex.shape[0] // 2
+#
+#     spectrum_p1x1 = spectra_complex[1]  # P1 X1 (L+R = I_1z, L-R = I_1z I_2z)
+#     ph_0_p1 = autophase_0(spectrum_p1x1, phase_width)
+#
+#     spectrum_p2x2 = spectra_complex[10]  # P2 X2 (L+R = I_2z, L-R = I_1z I_2z)
+#     ph_0_p2 = autophase_0(spectrum_p2x2, phase_width)
+#
+#     # if ("1000" in state_name) and ("CN" not in state_name):
+#
+#     spectra_complex[:l] = np.array([ng.proc_base.ps(s, p0=ph_0_p1 + 90, p1=0) for s in spectra_complex[:l]])
+#     spectra_complex[l:] = np.array([ng.proc_base.ps(s, p0=ph_0_p2 + 90, p1=0) for s in spectra_complex[l:]])
--- temporal_average.py
+++ temporal_average.py
@@ -6,21 +6,27 @@ from matplotlib import pyplot as plt
 from numpy.typing import NDArray
 from scipy.fft import fftfreq, fftshift
 
+from phase import auto_phase_all_spectra
+
 """ Specifying `ns` will now do the phase cycling averaging (assuming they're in the convention Donny has in his 
 notebook `ns4_array.ipynb`). """
 
+
 def temporal_average(
         data_folder: str,
+        AQ: float = 1.6,
+        PHASE_0: float = -44,
+        LEFT_SHIFT_INDEX: int = 0,
+        remove_digital: bool = False,
         ns: int | None = None,
+        auto_phase: bool = False,
+        phase_width: int = 150,
+        state_name: str = "1000",
         display_fid: bool = False,
         display_fft: bool = False,
         display_avg: bool = False,
         DISPLAY_I: int = 0,
-        XLIM_DISPLAY: tuple[float, float] = (-50, 50),
-        LEFT_SHIFT_INDEX: int = 63,
-        AQ: float = 1.6,  # Acquistion time (s)
-        PHASE_0: float = -44,
-        remove_digital: bool = False
+        XLIM_DISPLAY: tuple[float, float] = (-50, 50)
 ) -> tuple[NDArray, NDArray]:
     # SWH = 2000,  # Spectral Width [Hz]
     # SF = 202.4765750,  # Spectrometer frequency [Mhz]
@@ -38,13 +44,15 @@ def temporal_average(
                                   ACQ_TIME=AQ, XLIM_DISPLAY=XLIM_DISPLAY, LEFT_SHIFT_INDEX=LEFT_SHIFT_INDEX,
                                   remove_digital=remove_digital)[1]
         )
-
     ffts_proc = np.array(ffts_proc)
+
+    # Phase cycling:
     if ns is None:  # no phase cycling (the old usual method)
         assert ffts_proc.shape[:2] == (14, 3), "Data is not the expected shape of (14, 3, num_points)"
-    else:  # newly added method
-        assert ffts_proc.shape[0] == 14 * ns, (f"There should be 14*ns number of folders. Currently has:"
-                                               f" {ffts_proc.shape[0]}")
+        pass
+    else:  # newly added method for averaging different phase cycled spectra
+        assert ffts_proc.shape[0] == 14 * ns, \
+            f"There should be 14*ns number of folders. Currently has: {ffts_proc.shape[0]}"
         assert ffts_proc.shape[1] == 3
 
         ffts_averaged = np.zeros((14, 3, ffts_proc.shape[2]), dtype=ffts_proc.dtype)
@@ -52,28 +60,17 @@ def temporal_average(
             ffts_averaged[i] = np.mean(ffts_proc[i * ns: i * ns + ns], axis=0)
         ffts_proc = ffts_averaged
 
+    # Temporal 'Averaging'
     ffts_avg = np.mean(ffts_proc, axis=1)
 
+    # After `remove_digital`, the 0th order phase gets messed up (since it changes the left-shifting of the FID),
+    # so I have to manually phase the spectra again
+    if auto_phase:
+        auto_phase_all_spectra(ffts_avg, phase_width, state_name)
+
     if display_avg:
-        fig, axs = plt.subplots(nrows=4, ncols=1, sharex=True, sharey=True, figsize=(8, 12))
-        plot_complex(axs[0], freqs, ffts_proc[DISPLAY_I, 0, :])
-        plot_complex(axs[1], freqs, ffts_proc[DISPLAY_I, 1, :])
-        plot_complex(axs[2], freqs, ffts_proc[DISPLAY_I, 2, :])
-        plot_complex(axs[3], freqs, ffts_avg[DISPLAY_I, :])
-        axs[3].set_title("Averaged:")
+        plot_avg(freqs, ffts_avg, ffts_proc, DISPLAY_I, XLIM_DISPLAY)
 
-        for ax in axs.flat:
-            ax.grid(True)
-            ax.legend()
-            ax.set(xlabel="Freq (Hz)", ylabel="intensity (a.u.)")
-            ax.tick_params(direction='in')
-            ax.label_outer()
-            ax.set_xlim(XLIM_DISPLAY)
-
-        fig.tight_layout()
-        plt.show()
-
-    # print(ffts_avg.shape)
     return freqs, ffts_avg
 
 
@@ -84,7 +81,7 @@ def process_combined_data(
         PHASE_0: float = -40,
         ACQ_TIME: float = 1.6,
         XLIM_DISPLAY: tuple[float, float] = (-50, 50),
-        LEFT_SHIFT_INDEX: int = 63,
+        LEFT_SHIFT_INDEX: int = None,
         remove_digital: bool = False
 ) -> tuple[NDArray, NDArray]:
     dic_data, all_data = ng.fileio.bruker.read(data_folder)
@@ -98,6 +95,7 @@ def process_combined_data(
     #     all_data.append(ng.fileio.bruker.read_pdata(f"{data_folder}/pdata/{i}")[1])
 
     all_data = np.array(all_data)
+    # print(f"all_data.shape = {all_data.shape}")
     assert all_data.shape[0] == 4  # the first three arrays are data, and the last array is a placeholder!
     all_data = all_data[:3, :]
     # (Bruker needs them in factors of 2 or something)
@@ -117,26 +115,7 @@ def process_combined_data(
     freqs = fftshift(fftfreq(all_data.shape[1], d=ACQ_TIME / (all_data.shape[1] - 1)))
 
     if display_fft:
-        n_rows, n_cols = 3, 2
-        fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, sharex=True, sharey=True, figsize=(10, 8))
-        for i in range(n_rows):
-            plot_complex(axs[i, 0], freqs, ffts_raw[i, :])  # Raw spectra
-            plot_complex(axs[i, 1], freqs, ffts_proc[i, :])  # Processed spectra
-
-        for ax in axs.flat:
-            ax.grid(True)
-            ax.legend()
-            ax.set(xlabel="Freq (Hz)", ylabel="intensity (a.u.)")
-            ax.tick_params(direction='in')
-            ax.label_outer()
-            ax.set_xlim(XLIM_DISPLAY)
-            # ax.set_ylim(-2e10, 2e10)
-
-        axs[0, 0].set_title("Raw FFT")
-        axs[0, 1].set_title("Processed FFT (left shift)")
-
-        fig.tight_layout()
-        plt.show()
+        plot_fft(freqs, ffts_raw, ffts_proc, XLIM_DISPLAY)
 
     return freqs, ffts_proc
 
@@ -207,3 +186,44 @@ def test_run():
     DATA_FOLDER = r"C:\Users\lemon\OneDrive - Brown University\CNOT project\MIT\data\2024_12_13\1000_V6_NS4_2D_2ch"
     temporal_average(DATA_FOLDER, display_avg=True)
     return
+
+
+""" Plotting function """
+
+
+def plot_avg(freqs, ffts_avg, ffts_proc, DISPLAY_I, XLIM_DISPLAY):
+    fig, axs = plt.subplots(nrows=4, ncols=1, sharex=True, sharey=True, figsize=(8, 12))
+    plot_complex(axs[0], freqs, ffts_proc[DISPLAY_I, 0, :])
+    plot_complex(axs[1], freqs, ffts_proc[DISPLAY_I, 1, :])
+    plot_complex(axs[2], freqs, ffts_proc[DISPLAY_I, 2, :])
+    plot_complex(axs[3], freqs, ffts_avg[DISPLAY_I, :])
+    axs[3].set_title("Averaged:")
+    for ax in axs.flat:
+        ax.grid(True)
+        ax.legend()
+        ax.set(xlabel="Freq (Hz)", ylabel="intensity (a.u.)")
+        ax.tick_params(direction='in')
+        ax.label_outer()
+        ax.set_xlim(XLIM_DISPLAY)
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_fft(freqs, ffts_raw, ffts_proc, XLIM_DISPLAY):
+    n_rows, n_cols = 3, 2
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, sharex=True, sharey=True, figsize=(10, 8))
+    for i in range(n_rows):
+        plot_complex(axs[i, 0], freqs, ffts_raw[i, :])  # Raw spectra
+        plot_complex(axs[i, 1], freqs, ffts_proc[i, :])  # Processed spectra
+    for ax in axs.flat:
+        ax.grid(True)
+        ax.legend()
+        ax.set(xlabel="Freq (Hz)", ylabel="intensity (a.u.)")
+        ax.tick_params(direction='in')
+        ax.label_outer()
+        ax.set_xlim(XLIM_DISPLAY)
+        # ax.set_ylim(-2e10, 2e10)
+    axs[0, 0].set_title("Raw FFT")
+    axs[0, 1].set_title("Processed FFT (left shift)")
+    fig.tight_layout()
+    plt.show()
